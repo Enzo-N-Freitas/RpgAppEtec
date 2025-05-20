933d3bd766eac7b2204c4e12b719b34cf6e95960
unknown <enzo_negrao_freitas@outlook.com>
2025-05-20 20:44:11 -0300

    Geolocalização

---
+++ AppRpgEtec/ViewModels/Usuarios/ImagemUsuarioViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Services.Usuarios;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    public class ImagemUsuarioViewModel : BaseViewModel
+    {
+        private UsuarioService uService;
+
+        private static string conexaoAzureStorage = "Cole chave acesso conta armazenamento";
+
+        private static string container = "arquivos"; //nome do container criado
+
+        public ImagemUsuarioViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            uService = new UsuarioService(token);
+        }
+
+        private ImageSource fonteImagem;
+        public ImageSource FonteImagem
+        {
+            get { return fonteImagem; }
+            set
+            {
+                fonteImagem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private byte[] foto;
+        public byte[] Foto 
+        { 
+            get => foto; 
+            set
+            {
+                foto = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
